37c0bc56cf1a3708a74992e3a2d449dda800175d
Vizzyy <barnabas.vizy@gmail.com>
2021-01-01 19:39:57 -0500

    parameterize migration script

--- daemon.sh
+++ daemon.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+PYTHON_PATH=$1
+METRICS_PATH=$2
+
 echo "Assuming DB entry already made"
 echo "Crontab entries commented out"
 pip3 install schedule
+sed -i "s/<python>/$PYTHON_PATH/g" metrics.service
+sed -i "s/<metrics>/$METRICS_PATH/g" metrics.service
 sudo cp metrics.service /etc/systemd/system
 echo "Placed service file, be sure to verify python location"
 sudo systemctl daemon-reload
@@ -16,3 +21,4 @@ echo "alias statusmetrics='sudo systemctl status metrics'" >> /home/pi/.bash_ali
 echo "alias logmetrics='sudo journalctl -u metrics -f'" >> /home/pi/.bash_aliases
 source /home/pi/.bash_aliases
 echo "Done"
+bash
