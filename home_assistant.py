5beced6eab7bd24121f607d3d38016b7ddf22737
Vizzyy <barnabas.vizy@gmail.com>
2025-08-28 11:26:31 -0400

    attic sensor swap

--- home_assistant.py
+++ home_assistant.py
@@ -20,6 +20,8 @@ def get_ha_climate_data():
         temp_laundry = client.get_state(entity_id='sensor.titan_water_valve_actuator_air_temperature')
         temp_office = client.get_state(entity_id='sensor.office_temp_temperature')
         hum_office = client.get_state(entity_id='sensor.office_temp_humidity')
+        temp_attic = client.get_state(entity_id='sensor.attic_temp_temperature')
+        hum_attic = client.get_state(entity_id='sensor.attic_temp_humidity')
         temp_loft_nest = client.get_state(entity_id='sensor.loft_temperature')
         temp_master_nest = client.get_state(entity_id='sensor.master_bedroom_temperature')
         water_usage_daily = client.get_state(entity_id='sensor.water_usage_daily')
@@ -35,16 +37,18 @@ def get_ha_climate_data():
             'temp_loft_nest': temp_loft_nest.state,
             'temp_master_nest': temp_master_nest.state,
             'temp_lr_nest': temp_lr_nest.state,
+            'temp_attic': temp_attic.state,
             'hum_loft': hum_loft.state,
             'hum_guest': hum_guest.state,
             'hum_living_room': hum_living_room.state,
             'hum_master': hum_master.state,
             'hum_office': hum_office.state,
             'hum_lr_nest': hum_lr_nest.state,
+            'hum_attic': hum_attic.state,
             'water_usage_daily': water_usage_daily.state if water_usage_daily.state != 'unknown' else 0,
             'water_usage_weekly': water_usage_weekly.state if water_usage_weekly.state != 'unknown' else 0,
             'water_usage_monthly': water_usage_monthly.state if water_usage_monthly.state != 'unknown' else 0,
         }
 
 
-print(get_ha_climate_data())
+# print(get_ha_climate_data())
